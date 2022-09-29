d50008bfed3139ad8757f18af32e907a24d24779
Vasily Piccone <vasily.piccone@gmail.com>
2022-09-29 12:07:27 -0400

    Added functions for the solution to question1.

--- A3/main.py
+++ A3/main.py
@@ -9,13 +9,15 @@ if __name__ == "__main__":
     """
     Solution for Question 2
     """
+    print("Starting the solution for question 2...")
     # U238, Th234, Pro234, U234, Th230, Ra226, Rd222, Pl218, Pb214, Bs214, Pl214, Pb210, Bs210, Pl210 -> Then Lead
+    # Might make sense to rescale everything here, and then rescale to retrive the answer
     u238 = (365*24*60*60)*4468*10**9
     Th234 = 24.10*24*60*60
     Pr234 = 6.70*60*60
     u234 = 245500*(365*24*60*60)
     Th230 = 75380*(365*24*60*60)
-    Ra226 = 1600**(365*24*60*60)
+    Ra226 = 1600*(365*24*60*60)
     Rd222 = 3.8235*24*60*60
     Po218 = 3.10*60
     Pb214 = 26.8*60
@@ -23,10 +25,10 @@ if __name__ == "__main__":
     Po214 = 164.3*10**(-6)
     Pb210 = 22.3*365*24*60*60
     Bi210 = 5015.0*365*24*60*60
-    Po210 = 138376*60*60
+    Po210 = 138.376*24*60*60
 
     halflife = [u238, Th234, Pr234, u234, Th230, Rd222, Po218, Pb214, Bi214, Po214, Pb210, Bi210, Po210] # In seconds
     lamb = [np.log(2)/(halflife[i]) for i in range(len(halflife))]
-    
+
     print(lamb)
     b = decay_solver(lamb)
--- A3/utils.py
+++ A3/utils.py
@@ -13,9 +13,20 @@ def rk4_step(fun, x, y, h):
 
 def rk4_stepd(fun, x, y, h):
     # Compares two steps of length h/2, and uses them to cancel leading-order error term from RK4
+    k1, k2, k3, k4 = h*fun(x,y), h*fun(x+h/2,y+k1/2), h*fun(x+h/2,y+k2/2), h*fun(x+h,y+k3)
+    dy1 =( k1+2*k2+2*k3+k4)/6
+    y1 = y+dy1
 
-    # Count function evals
-    pass
+    # RK4 for a h/2 step size
+    k5, k6, k7, k8 = (k1/2), h*fun(x+h/4, y+k5/2)/2, h*fun(x+h/4, y+k6/2)/2, h*fun(x+h/2, y+k7)/2
+    dy2 = (k5+2*k6+2*k7+k8)/6 
+    temp_y = y + dy2
+
+    k9, k10, k11, k12 = h*fun(x+h/2, temp_y)/2, h*fun(x+3*h/4, temp_y+k9/2)/2, h*fun(x+3*h/4, temp_y+k10/2)/2, h*fun(x+h, temp_y+k11)/2
+    dy3 = (k9+2*k10+2*k11+k12)/6
+    y2 = temp_y + dy3
+
+    return y2 +(y2-y1)/15
 
 """
 Function for Question 2
@@ -29,23 +40,30 @@ def decay_solver(coeffs):
     mat = np.diag(diags) + np.diag(lower_diags, -1)
     
     # Solve the equations (IVPs)
-    iv = np.zeros(n, 1)
+    iv = np.zeros(n)
     iv[0] = 1
     print(iv)
 
     # Create logspace (because the decay times are so different)
     time = np.logspace(-6, 20, 101)
+    print(time)
 
     # Solve
     F = lambda t, s: np.dot(mat, s)
-    sol = solve_ivp(F, [10**(-6), 10**(20)], iv, t_eval=time)
+    sol = solve_ivp(F, [10**(-6), 10**(20)], iv, method='Radau', t_eval=time)
     # make plots?
     plt.figure(figsize = (12, 8))
-    plt.plot(sol.y.T[:, 0], sol.y.T[:, 1])
+    plt.plot(time, sol.y.T[:,0]/sol.y.T[])
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
 
-
-
     return 0
+
+"""
+Functions for Question 3
+"""
+
+def photogrammetry():
+
+    pass

ed4103b02320e80e79eaa60708f0fc9efbe39dc6
Vasily Piccone <vasily.piccone@gmail.com>
2022-09-30 18:28:06 -0400

    Final assignment submission

--- A3/main.py
+++ A3/main.py
@@ -4,12 +4,28 @@ if __name__ == "__main__":
     """
     Solution for Question 1
     """
+    h = 0.2
+    x = np.linspace(-20, 20, 200)
+    y0 = 1
 
+    rhs = lambda x, y: y/(1+x**2)
+
+    sol1 = [y0]
+    sol2 = [y0]
+    analytic = 1/(np.exp(np.arctan(-20)))*np.exp(np.arctan(x))
+    for i in range(1, len(x)):
+        sol1.append(rk4_step(rhs, x[i], sol1[i-1], h)) 
+        sol2.append(rk4_stepd(rhs, x[i], sol2[i-1], h))
+
+    err1 = analytic - sol1
+    err2 = analytic - sol2
+    # plt.plot(x, err1, x, err2)
+    # plt.legend(['sol1 error', 'sol2 error'], loc="upper left")
+    # plt.show()
 
     """
     Solution for Question 2
     """
-    print("Starting the solution for question 2...")
     # U238, Th234, Pro234, U234, Th230, Ra226, Rd222, Pl218, Pb214, Bs214, Pl214, Pb210, Bs210, Pl210 -> Then Lead
     # Might make sense to rescale everything here, and then rescale to retrive the answer
     u238 = (365*24*60*60)*4468*10**9
@@ -25,10 +41,21 @@ if __name__ == "__main__":
     Po214 = 164.3*10**(-6)
     Pb210 = 22.3*365*24*60*60
     Bi210 = 5015.0*365*24*60*60
-    Po210 = 138.376*24*60*60
+    Po210 = 138376*24*60*60
+    Pb206 = (365*24*60*60)*4468*10**9
 
-    halflife = [u238, Th234, Pr234, u234, Th230, Rd222, Po218, Pb214, Bi214, Po214, Pb210, Bi210, Po210] # In seconds
+    halflife = [u238, Th234, Pr234, u234, Th230, Rd222, Po218, Pb214, Bi214, Po214, Pb210, Bi210, Po210, Pb206] # In seconds
     lamb = [np.log(2)/(halflife[i]) for i in range(len(halflife))]
 
-    print(lamb)
-    b = decay_solver(lamb)
+    # b = decay_solver(lamb) # Uncomment to get the results. Look at the function "decay_solver" to change between plot a and b
+
+    """
+    Solution to Question 3
+    """
+    path = "./dish_zenith.txt"
+    a, x0, y0, z0, cov = photogrammetry(path)
+    print(a, x0, y0, z0)
+    print(cov)
+    uncert_a = cov[0,0]
+    f, f_err = 1/(4*a), np.sqrt(uncert_a/(4*a**2))
+    print(f, f_err)
--- A3/utils.py
+++ A3/utils.py
@@ -7,22 +7,34 @@ Functions for Question 1
 """
 # Code provided to us in class
 def rk4_step(fun, x, y, h):
-    k1, k2, k3, k4 = h*fun(x,y), h*fun(x+h/2,y+k1/2), h*fun(x+h/2,y+k2/2), h*fun(x+h,y+k3)
+    k1 = h*fun(x,y)
+    k2 = h*fun(x+h/2,y+k1/2)
+    k3 = h*fun(x+h/2,y+k2/2)
+    k4 = h*fun(x+h,y+k3)
     dy=(k1+2*k2+2*k3+k4)/6
     return y+dy
 
 def rk4_stepd(fun, x, y, h):
     # Compares two steps of length h/2, and uses them to cancel leading-order error term from RK4
-    k1, k2, k3, k4 = h*fun(x,y), h*fun(x+h/2,y+k1/2), h*fun(x+h/2,y+k2/2), h*fun(x+h,y+k3)
-    dy1 =( k1+2*k2+2*k3+k4)/6
+    k1 = h*fun(x,y)
+    k2 = h*fun(x+h/2,y+k1/2)
+    k3 = h*fun(x+h/2,y+k2/2)
+    k4 = h*fun(x+h,y+k3)
+    dy1 =(k1+2*k2+2*k3+k4)/6
     y1 = y+dy1
 
     # RK4 for a h/2 step size
-    k5, k6, k7, k8 = (k1/2), h*fun(x+h/4, y+k5/2)/2, h*fun(x+h/4, y+k6/2)/2, h*fun(x+h/2, y+k7)/2
+    k5 = (k1/2)
+    k6 = h*fun(x+h/4, y+k5/2)/2
+    k7 = h*fun(x+h/4, y+k6/2)/2
+    k8 = h*fun(x+h/2, y+k7)/2
     dy2 = (k5+2*k6+2*k7+k8)/6 
     temp_y = y + dy2
 
-    k9, k10, k11, k12 = h*fun(x+h/2, temp_y)/2, h*fun(x+3*h/4, temp_y+k9/2)/2, h*fun(x+3*h/4, temp_y+k10/2)/2, h*fun(x+h, temp_y+k11)/2
+    k9 = h*fun(x+h/2, temp_y)/2
+    k10 = h*fun(x+3*h/4, temp_y+k9/2)/2
+    k11 = h*fun(x+3*h/4, temp_y+k10/2)/2
+    k12 = h*fun(x+h, temp_y+k11)/2
     dy3 = (k9+2*k10+2*k11+k12)/6
     y2 = temp_y + dy3
 
@@ -42,18 +54,17 @@ def decay_solver(coeffs):
     # Solve the equations (IVPs)
     iv = np.zeros(n)
     iv[0] = 1
-    print(iv)
 
     # Create logspace (because the decay times are so different)
     time = np.logspace(-6, 20, 101)
-    print(time)
 
     # Solve
     F = lambda t, s: np.dot(mat, s)
     sol = solve_ivp(F, [10**(-6), 10**(20)], iv, method='Radau', t_eval=time)
     # make plots?
     plt.figure(figsize = (12, 8))
-    plt.plot(time, sol.y.T[:,0]/sol.y.T[])
+    # plt.semilogx(time, sol.y.T[:,-1]/sol.y.T[:,0]) # For Lead over Uranium concentrations
+    plt.semilogx(time, sol.y.T[:,4]/sol.y.T[:,3]) # For Thorium vs Polonium (part b)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
@@ -64,6 +75,27 @@ def decay_solver(coeffs):
 Functions for Question 3
 """
 
-def photogrammetry():
+def photogrammetry(path):
+    # Load position data from txt file
+    x, y, z = np.loadtxt(path).T
 
-    pass
+    # Define Am matrix
+    size = len(x)
+    Am = np.zeros((size, 4))
+    Am[:,0], Am[:,1], Am[:,2], Am[:,3] = x, y, x**2 +y**2, np.ones(size)
+
+    # Linear least squares model from slides
+    m = np.linalg.inv(Am.T@Am)@Am.T@z
+
+    # Solving for the actual parameters
+    a, x0, y0 = m[2], -m[0]/(2*m[2]), -m[1]/(2*m[2])
+    z0 = m[3] - a*(x0**2+y0**2)
+
+    # Calculating estimated error based off of what we did in class
+    noise_diag = z - Am@m
+    N = np.diag(noise_diag)
+
+    # Compute covariance matrix
+    cov = np.linalg.inv(Am.T@np.linalg.inv(N)@Am)
+
+    return a, x0, y0, z0, cov
